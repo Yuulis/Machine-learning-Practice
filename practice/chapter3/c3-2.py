86aa0fd54d9467d360a4e66c4773476c85411121
Yuulis <yuulis2005@gmail.com>
2022-04-05 16:00:56 +0900

    EarlyStoppingを用いて過学習防止

--- practice/chapter3/c3-2.py
+++ practice/chapter3/c3-2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.callbacks import EarlyStopping
 
 # グラフ表示設定
 def plt_settings():
@@ -22,27 +23,44 @@ plt.rcParams.update(config)
 
 
 n = 50
+n_train = 30
 noise = 10
-np.random.seed(2)
+np.random.seed(5)
 
 xs = np.random.rand(n) * 30
 ts = (xs * 0.1) ** 4 + 1 * xs + 50 + np.random.randn(n) * noise
+xs_train = xs[:n_train].reshape(-1, 1)
+ts_train = ts[:n_train].reshape(-1, 1)
+xs_test = xs[n_train:].reshape(-1, 1)
+ts_test = ts[n_train:].reshape(-1, 1)
+
 x_range = (0, 30)
-t_range = (0, 200)
+t_range = (0, 250)
+
+n_batch = len(xs)
+
+# Early Stopping
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=10,
+)
 
 # モデル生成
 model = Sequential([
     Dense(32, activation='sigmoid', input_dim=1),
+    # Dense(32, activation='relu', input_dim=1),
     Dense(1, activation='linear')
 ])
 model.compile(optimizer='adam', loss='mse')
 
 # 学習
 history = model.fit(
-    xs.reshape(-1, 1),
-    ts.reshape(-1, 1),
-    batch_size=len(xs),
-    epochs=10000,
+    xs_train,
+    ts_train,
+    validation_data=(xs_test, ts_test),
+    batch_size=n_train,
+    epochs=40000,
+    callbacks=[early_stopping],
     verbose=0,
 )
 
@@ -51,17 +69,23 @@ xs_pred = np.linspace(x_range[0], x_range[1], 100)
 ys_pred = model.predict(xs_pred.reshape(-1, 1))
 
 # 学習曲線表示
-print(f"MSE = {history.history['loss'][-1]:.3f}")
+print(f"train_MSE = {history.history['loss'][-1]:.3f}")
+print(f"test_MSE = {history.history['val_loss'][-1]:.3f}")
+print(f"min test_MSE : {np.argmin(history.history['val_loss']):d} epochs")
 print('The learning curve was output.\n')
-plt.plot(history.history['loss'])
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
 plt.title('MSE')
 plt.xlabel('epochs')
+plt.legend()
 plt.grid(axis='both')
 plt.show()
 
 # グラフ表示
 print('The data and a neural network model for it were output.\n')
-plt.scatter(xs, ts, label='data')
+plt.scatter(xs_train, ts_train, label='train')
+plt.scatter(xs_test, ts_test, label='test')
 plt.plot(xs_pred, ys_pred, 'green', label='neural network model')
 
 plt_settings()
